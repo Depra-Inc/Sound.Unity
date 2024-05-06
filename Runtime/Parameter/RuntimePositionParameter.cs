5879edc2e06a573543178e7c0939b04100337dee
godzzz <g0dzZz1lla@yandex.ru>
2024-05-06 19:34:22 +0400

    [v0.0.15] Runtime position parameter

---
+++ Runtime/Parameter/RuntimePositionParameter.cs
@@ -0,0 +1,12 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2024 Nikolay Melnikov <n.melnikov@depra.org>
+
+using System;
+using Depra.SerializeReference.Extensions;
+
+namespace Depra.Sound.Parameter
+{
+	[Serializable]
+	[SerializeReferenceIcon("d_Transform Icon")]
+	public readonly struct RuntimePositionParameter : IAudioClipParameter { }
+}
