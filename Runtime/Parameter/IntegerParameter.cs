8c0ee96b4416aa8c4fac41939efe338fa0a4647c
godzzz <g0dzZz1lla@yandex.ru>
2024-07-06 02:21:16 +0400

    [v0.0.23] Fix for parameters serialization

--- Runtime/Parameter/IntegerParameter.cs
+++ Runtime/Parameter/IntegerParameter.cs
@@ -1,14 +1,18 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
 // © 2024 Nikolay Melnikov <n.melnikov@depra.org>
 
+using System;
 using UnityEngine;
 
 namespace Depra.Sound.Parameter
 {
+	[Serializable]
 	public sealed class IntegerParameter : IAudioClipParameter
 	{
 		[field: SerializeField] public string Name { get; private set; }
 		[field: SerializeField] public int Value { get; private set; }
+
+		public IntegerParameter() { }
 
 		public IntegerParameter(string name, int value)
 		{
--- Runtime/Parameter/LabelParameter.cs
+++ Runtime/Parameter/LabelParameter.cs
@@ -1,16 +1,20 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
 // © 2024 Nikolay Melnikov <n.melnikov@depra.org>
 
+using System;
 using Depra.SerializeReference.Extensions;
 using UnityEngine;
 
 namespace Depra.Sound.Parameter
 {
+	[Serializable]
 	[SerializeReferenceIcon("d_FilterByLabel")]
 	public sealed class LabelParameter : IAudioClipParameter
 	{
 		[field: SerializeField] public string Name { get; private set; }
 		[field: SerializeField] public string Value { get; private set; }
+
+		public LabelParameter() { }
 
 		public LabelParameter(string name, string value)
 		{
